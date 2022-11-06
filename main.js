5636658cbabbe2c52d7231d787f10111d5551f52
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2022-11-06 15:38:27 -0300

    jsupdate

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+function createGame(){
+    return`
+    <li>
+        <img src="assets/icon-brazil.svg" alt="Brazil">
+        <strong>07:00</strong>
+        <img src="assets/icon-cameroon.svg" alt="Cameroon">
+    </li>`
+}
+
+function createCard(date,day,games){
+    return `
+    <div class="card">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>
+        ${games}
+    </ul>
+</div>`
+}
+
 document.querySelector('#app').innerHTML=`
 <header>
-            <img src="assets/logo.svg" alt="logo do catar">
-        </header>
-
-        <main id="cards">
-            <div class="card">
-                <h2>24/11 <span>quinta</span></h2>
-                <ul>
-                    <li>
-                        <img src="assets/icon-brazil.svg" alt="Brazil">
-                        <strong>07:00</strong>
-                        <img src="assets/icon-cameroon.svg" alt="Cameroon">
-                    </li>
-                    <li>
-                        <img src="assets/icon-brazil.svg" alt="Brazil">
-                        <strong>07:00</strong>
-                        <img src="assets/icon-cameroon.svg" alt="Cameroon">
-                    </li>
-                </ul>
-            </div>
-        </main>
+    <img src="assets/logo.svg" alt="logo do catar">
+</header>
+<main id="cards">
+    ${createCard("24/11","quinta",createGame())}
+    ${createCard("28/11","segunda",createGame())}
+    ${createCard("02/12","sexta",createGame())}
+</main>
 `
